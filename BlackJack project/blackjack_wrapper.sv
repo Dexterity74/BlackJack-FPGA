d2ea9e6af4e66f9a50fa6df6c00a0c725e84b7d6
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-30 23:41:03 -0700

    added io pins

--- BlackJack project/blackjack_wrapper.sv
+++ BlackJack project/blackjack_wrapper.sv
@@ -3,3 +3,27 @@
 	created: 07/30/21
 	team name: POWER_OF_TWO
 */
+
+module blackjack_wrapper
+	(	
+		input  	logic       	CLOCK_50,
+      	input  	logic [17: 0] 	SW,
+		input 	logic [3: 0] 	KEY,
+		output 	logic [17: 0] 	LEDR,
+		output 	logic [7: 0] 	LEDG,
+		output 	logic [6: 0]	HEX0,
+		output 	logic [6: 0]	HEX1,
+		output 	logic [6: 0]	HEX2,
+		output 	logic [6: 0]	HEX3,
+		output 	logic [6: 0]	HEX4,
+		output 	logic [6: 0]	HEX5,
+		output 	logic [6: 0]	HEX6,
+		output 	logic [6: 0]	HEX7
+	);
+
+	//TODO: fill in interface
+	//blackjackGame theGame(CLOCK_50);
+
+	//all unused pins should be driven low (off)
+
+endmodule
