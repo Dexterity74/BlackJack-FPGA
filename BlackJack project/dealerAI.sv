3c07fffeb2aa3b8c29e2b9553b27aeee575e3412
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 01:22:13 -0700

    dealer logic that won't compile

--- BlackJack project/dealerAI.sv
+++ BlackJack project/dealerAI.sv
@@ -1,5 +1,34 @@
-/*	file name:
+/*	file name: dealerAI
 	Authors: Justin Negron and Richard Osborn
 	created: 07/30/21
 	team name: POWER_OF_TWO
+
+	drive turnIndicator to indicate the AI should make a decision
+
+	dealer stands when hand >= 17;
+
 */
+
+module dealerAI
+	(
+		input 	logic			turnIndicator,
+		input	logic	[4 : 0] handValue,
+
+		output 	blackjack::gameCommand 	command
+	);
+
+	//TODO more complex logic
+	always_comb
+	begin
+		if(turnIndicator)
+		begin	
+			if(handValue < 17) command = HIT;
+			else if command = STAND;
+		end
+		else command = NONE;
+		
+	end
+
+	//assign command <= HIT;
+
+endmodule
