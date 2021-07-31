9067746e901a70b3cd7a5dd2fd98dd28914bad5e
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 02:04:12 -0700

    cleaned up

--- BlackJack project/counter.sv
+++ BlackJack project/counter.sv
@@ -2,13 +2,23 @@
 	Authors: Justin Negron and Richard Osborn
 	created: 07/30/21
 	team name: POWER_OF_TWO
+
+	description:
+	this is a counter that counts from 0 to TOP. 
+
+	One can read the current VALUE of the counter.
+
+	HITTOP will be asserted for 1 clock cycle.
+
+	enable low will freeze counter in place.
 */
 
 module counter
 	#(parameter WIDTH = 16)
 	(
-		input	logic	clk,
-		input	logic	reset,
+		input	logic	clk,	//ticker
+		input	logic	reset,	//hi means reset
+		input	logic 	enable,	//hi means enabled
 		input	logic	[WIDTH - 1 : 0]	top,
 
 		output	logic	hitTop, //asserted for one signal while 
@@ -19,11 +29,15 @@ module counter
 
 	always @ (posedge clk or posedge _hitTop) 
 	begin
-		if(reset || _hitTop) 
-			value <= 0; 
+		if(enabled)
+		begin
+			if(reset || _hitTop) 
+				value <= 0; 
+			else
+				value <= value + 1;//increment counter
+		end
 		else
-			value <= value + 1;//increment counter
-
+			value <= value;
 	end
 
 	assign _hitTop = (value == top);//internal value for feedback
