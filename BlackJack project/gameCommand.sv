535913585e606357d37a91dfa6b85182d2478986
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-31 01:22:04 -0700

    typedefs that won't compile because  Idon't know why

---
+++ BlackJack project/gameCommand.sv
@@ -0,0 +1,10 @@
+/*	file name: dealerAI
+	Authors: Justin Negron and Richard Osborn
+	created: 07/30/21
+	team name: POWER_OF_TWO
+*/
+
+//custom signal type
+package blackjack;
+typedef enum logic [2:0] {NONE, HIT, STAND, DOUBLE} gameCommand;
+endpackage
---
+++ BlackJack project/hand.sv
@@ -0,0 +1,16 @@
+/*	file name: dealerAI
+	Authors: Justin Negron and Richard Osborn
+	created: 07/30/21
+	team name: POWER_OF_TWO
+
+	describes a hand of cards
+
+*/
+
+//wtf??? why doesn't this work?
+
+
+package blackjack;
+
+typedef logic [4 : 0] hand;
+endpackage
