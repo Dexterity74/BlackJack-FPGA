2fd1ac1d5d9eb3af0f228392434f8131c083b9e6
Dexterity74 <justin.704504@gmail.com>
2021-08-06 15:17:51 -0700

    relative input/output for game to wrapper
    This I filled the game with input/output variables, the names I just thought up on the fly, they can be changed. This is just so it can be uploaded to the board to test whenever need be.

--- BlackJack project/blackjack_wrapper.sv
+++ BlackJack project/blackjack_wrapper.sv
@@ -6,23 +6,29 @@
 
 module blackjack_wrapper
 	(	
-		input  	logic       	CLOCK_50,
-      	input  	logic [17: 0] 	SW,
-		input 	logic [3: 0] 	KEY,
-		output 	logic [17: 0] 	LEDR,
+		input  	logic       	CLOCK_50, // clk
+      	input  	logic [17] 	    SW,		  // reset (limiting to [17] to try and line up input/outputs to blackjackGame.sv)
+		input 	logic [1: 0] 	KEY,      // hit/stand
+		output 	logic [17: 0] 	LEDR,     
 		output 	logic [7: 0] 	LEDG,
-		output 	logic [6: 0]	HEX0,
-		output 	logic [6: 0]	HEX1,
-		output 	logic [6: 0]	HEX2,
-		output 	logic [6: 0]	HEX3,
-		output 	logic [6: 0]	HEX4,
-		output 	logic [6: 0]	HEX5,
-		output 	logic [6: 0]	HEX6,
-		output 	logic [6: 0]	HEX7
+		output 	logic [0: 6]	HEX0,
+		output 	logic [0: 6]	HEX1,
+		output 	logic [0: 6]	HEX2,
+		output 	logic [0: 6]	HEX3,
+		output 	logic [0: 6]	HEX4,
+		output 	logic [0: 6]	HEX5,
+		output 	logic [0: 6]	HEX6,
+		output 	logic [0: 6]	HEX7
 	);
 
 	//TODO: fill in interface
-	//blackjackGame theGame(CLOCK_50);
+	//blackjackGame theGame(CLOCK_50, SW[17], KEY[0], KEY[1], LEDR[17: 0], 
+							LEDG[7: 0], HEX0[0: 6], HEX1[0: 6], HEX2[0: 6],
+							HEX3[0: 6], HEX4[0: 6], HEX5[0: 6], HEX6[0: 6],
+							HEX7[0: 6]);
+
+	// Justin: I don't think switch 17 will be used for gameplay at all so
+			   it would make good for a reset switch
 
 	//all unused pins should be driven low (off)
 
