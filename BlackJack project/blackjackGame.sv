2acb50293a8ee20b67990bec7c280ee4cfb45563
Dexterity74 <justin.704504@gmail.com>
2021-08-06 19:48:26 -0700

    fixing output logic locations

--- BlackJack project/blackjackGame.sv
+++ BlackJack project/blackjackGame.sv
@@ -9,15 +9,5 @@ module blackjackGame
 		input  logic 	      clk,
 		input  logic 	      reset,
 		input  logic 		  hit, stand,
-		output logic [17: 0]   redLights,
-		output logic [7: 0]    greenLights,
-		output logic [6: 0]    hex0, 
-		output logic [6: 0]    hex1,
-		output logic [6: 0]    hex2,  
-		output logic [6: 0]    hex3,   // hex3-0 is BLJK, TIE, WIN, LOSE display
-		output logic [6: 0]    hex4,
-		output logic [6: 0]    hex5,   // hex5-4 is dealer_hand display
-		output logic [6: 0]    hex6,
-		output logic [6: 0]    hex7,   // hex7-6 is player_hand display
 	);
 endmodule
