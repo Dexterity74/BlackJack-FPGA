1866882f45ffd38fffe97e96d7e2172d9cc3adb0
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-06 20:41:59 -0700

    new card files

---
+++ BlackJack project/card.svh
@@ -0,0 +1,10 @@
+/*	file name: card.svh
+	Authors: Justin Negron and Richard Osborn
+	created: 08/06/21
+	team name: POWER_OF_TWO
+
+	signal that represents card values
+*/
+
+//custom signal type
+typedef logic [3:0] card;
---
+++ BlackJack project/cardDeck.sv
@@ -0,0 +1,44 @@
+/*	file name: cardDeck.sv
+	Authors: Justin Negron and Richard Osborn
+	created: 08/06/21
+	team name: POWER_OF_TWO
+
+    holds data for a deck of cards
+    returns 1 - 10 (1 is ace)
+
+    has no concept of suit
+
+    will need to circle back if we want to draw a King of Clubs.
+*/
+
+`include "card.svh"
+
+module cardDeck
+    (
+        input logic     clk,
+        input logic     requestCard,
+
+        output card     dealtCard
+    );
+
+    card _rngValue;
+
+    //modules
+    randomNumberGenerator #(3) rng(clk, requestCard, 'h12, _rngValue);
+
+    //logic
+    always_comb
+    begin
+        if(requestCard)
+        begin
+            case(card)
+                'h10:       dealtCard = 'hA;//jack
+                'h11:       dealtCard = 'hA;//queen
+                'h12:       dealtCard = 'hA;//king
+                default:    dealtCard = _rngValue + 1; //all else
+            endcase 
+        end
+        else    dealtCard = -1; //invalid card amount
+    end
+
+endmodule
