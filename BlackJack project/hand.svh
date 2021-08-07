16974a0a2d8b5c7dd1fba38b29c4a98416c0be91
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-06 21:15:38 -0700

    Update hand.svh

--- BlackJack project/hand.svh
+++ BlackJack project/hand.svh
@@ -4,15 +4,10 @@
 	team name: POWER_OF_TWO
 
 	describes a hand of cards
+	represents the sum of all the cards in the player's hand.
+	must cover [0, 30] //worst hand is three 10s.
+	2 ^ 5 = 32
 
 */
 
-//wtf??? why doesn't this work?
-
-
-//package blackjack;
-//represents the sum of all the cards in the player's hand.
-//must cover [0, 30] //worst hand is three 10s.
-//2 ^ 5 = 32
 typedef logic [4 : 0] hand;
-//endpackage
