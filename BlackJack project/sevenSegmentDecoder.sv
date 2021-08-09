2f5ee58399fd49dd9334d5b325ab0a8db2335463
Dexterity74 <justin.704504@gmail.com>
2021-08-09 13:43:43 -0700

    quick updates

--- BlackJack project/sevenSegmentDecoder.sv
+++ BlackJack project/sevenSegmentDecoder.sv
@@ -7,7 +7,7 @@
 // WHEN WE CREATE THE WRAPPER, REFER TO HEX LOGIC AS HEX3[0:6], NOT HEX3[6:0]    
 
 module sevenSegmentDecoder(input  logic [5:0] data,
-                	   output logic [6:0] segments);
+                	       output logic [6:0] segments);
 
   	always_comb
     	case (data)
