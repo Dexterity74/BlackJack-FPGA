a58461a960ff69b90bdb083a2650ce9ff4a87861
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-09 15:43:08 -0700

    Update blackjack_testbench.sv

--- BlackJack project/blackjack_testbench.sv
+++ BlackJack project/blackjack_testbench.sv
@@ -12,22 +12,28 @@
 `include "hand.svh"
 `include "turnIndicator.svh"
 
+`define HIT_KEY 0
+`define STAND_KEY 1
+`define DEAL_CARD_KEY 2
+`define KEY_STATE_UP 1
+`define KEY_STATE_DOWN 0
+
 module blackjack_testbench
     ();
     //device inputs
     logic clk;
     logic reset;
-    logic key0;
-    logic key1;
+    logic hitKey;
+    logic standKey;
 
     //device outputs
-    hand playerHand;
-    hand dealerHand;
-    gameState gameState;
-    turnIndicator whoseTurnIsIt;
+    `hand playerHand;
+    `hand dealerHand;
+    `gameState gameState;
+    `turn whoseTurnIsIt;
 
-    blackjackGame dut(clk, reset, {key1, key0}, 
-        playerHand, dealerHand, gameState, whoseTurnIsIt);
+    blackjackGame dut(clk, reset, {standKey, hitKey}, 
+        playerHandSum, dealerHandSum, gameState, whoseTurnIsIt);
 
     always
     begin
@@ -36,33 +42,108 @@ module blackjack_testbench
 
     initial
     begin
+        //initialize
+        #1;
         clk = 0;
         reset = 0;
-        key0 = 0;
-        key1 = 0;
+        hitKey = `KEY_STATE_UP;
+        standKey = `KEY_STATE_UP;
+        #1;
 
+        //now test 1 hand
+
+        //deal dealer 1st card
+        #5;
+
+        //deal dealer 2nd card 
+        #5;
+
+        //check blackjack (most likely not blackjack)
+        #5;
+
+        //S_DEALERS_TURN (not)
+        #5;
+
+        //deal first card to player
+        #5;
+
+        //deal second card to player
+        #5;
+
+        //check player bj
+        #5; //probably has 4, 5 or something
+
+        //check player bust
+        #5;
+
+        //check player 5CC
+        #5; //should absolutely be false
+
+        //now wait for player's choice
+        #18;
+
+        hitKey = KEY_STATE_DOWN;
+        #5;
+        hitKey = KEY_STATE_UP;
+
+        //S-Deal_player card
+        #5;
+
+        //check bj
+        #5;
+
+        //check bust
+        #5;
+
+        //check charlie
+        #5;
+
+        //wait on input again (try to bust or charlie)
+        
+        //now wait for player's choice
+        #18;
+
+        hitKey = KEY_STATE_DOWN;
+        #5;
+        hitKey = KEY_STATE_UP;
+        
+        //now wait for player's choice
+        #18;
+
+        hitKey = KEY_STATE_DOWN;
+        #5;
+        hitKey = KEY_STATE_UP;
+        
+        //now wait for player's choice
+        #18;
+
+        hitKey = KEY_STATE_DOWN;
+        #5;
+        hitKey = KEY_STATE_UP;
+        
+        //now wait for player's choice
+        #18;
+
+        hitKey = KEY_STATE_DOWN;
+        #5;
+        hitKey = KEY_STATE_UP;
+        
+        //now wait for player's choice
+        #18;
+
+        hitKey = KEY_STATE_DOWN;
+        #5;
+        hitKey = KEY_STATE_UP;
+        
+        //now wait for player's choice
+        #18;
+
+        hitKey = KEY_STATE_DOWN;
+        #5;
+        hitKey = KEY_STATE_UP;
+
+    
     end
 
-    // logic [6: 0] hexXOut;
-    // logic [6: 0] TIE_T;
-    // logic [6: 0] TIE_T;
-    // assign TIE_T = 'b100010;
-
-
-    // logic [6: 0] hex0_internalVariable;
-    // logic [6: 0] hex1_internalVariable;
-
-    // logic [3:0] hand;
-
-
-    // always_comb 
-    // begin
-    //     if(gameState)
-    //         internalVariable = TIE_T;
-        
-        
-    // end
-
-    // assign hexXOut = internalVariable;
     
 endmodule
