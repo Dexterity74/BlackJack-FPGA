e565e84dcb2c8141fc6f693be63ff0d2aca5e938
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-11 02:50:53 -0700

    tied down LEDs

--- BlackJack project/blackjack_wrapper.sv
+++ BlackJack project/blackjack_wrapper.sv
@@ -13,10 +13,10 @@
 module blackjack_wrapper
 	(	
 		input  	logic       	CLOCK_50, // clk
-      	input  	logic [17: 0] 	    SW,		  // reset (limiting to [17] to try and line up input/outputs to blackjackGame.sv)
+      	input  	logic [17: 0] 	SW,		  // reset (limiting to [17] to try and line up input/outputs to blackjackGame.sv)
 		input 	logic [2: 0] 	KEY,      // hit/stand
-		//output 	logic [17: 0] 	LEDR,     
-		//output 	logic [7: 0] 	LEDG,
+		output 	logic [17: 0] 	LEDR,     
+		output 	logic [7: 0] 	LEDG,
 		output 	logic [0: 6]	HEX0,
 		output 	logic [0: 6]	HEX1,
 		output 	logic [0: 6]	HEX2,
@@ -32,13 +32,13 @@ module blackjack_wrapper
 	`gameState gameState, nextstate;
 	`turn turnTracker;
 
+	assign LEDR = 0;
+	assign LEDG = 0;
+
 	//TODO: fill in interface
 	blackjackGame theGame(CLOCK_50, SW[17], {KEY[2], KEY[1], KEY[0]}, 
 						  playerHandSum, dealerHandSum, gameState, turnTracker); 
 	
-	
-
-
 	outputController oC(dealerHandSum, playerHandSum, gameState, HEX7[0: 6], HEX6[0: 6], HEX5[0: 6], HEX4[0: 6], 
 							HEX3[0: 6], HEX2[0: 6], HEX1[0: 6], HEX0[0: 6]);
 
