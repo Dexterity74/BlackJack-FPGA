c82bbf806728ab618462f1afeef6ab639c7532b1
Dexterity74 <justin.704504@gmail.com>
2021-08-11 02:09:30 -0700

    output to wrapper

--- BlackJack project/blackjack_wrapper.sv
+++ BlackJack project/blackjack_wrapper.sv
@@ -14,9 +14,9 @@ module blackjack_wrapper
 	(	
 		input  	logic       	CLOCK_50, // clk
       	input  	logic [17] 	    SW,		  // reset (limiting to [17] to try and line up input/outputs to blackjackGame.sv)
-		input 	logic [1: 0] 	KEY,      // hit/stand
-		output 	logic [17: 0] 	LEDR,     
-		output 	logic [7: 0] 	LEDG,
+		input 	logic [2: 0] 	KEY,      // hit/stand
+		//output 	logic [17: 0] 	LEDR,     
+		//output 	logic [7: 0] 	LEDG,
 		output 	logic [0: 6]	HEX0,
 		output 	logic [0: 6]	HEX1,
 		output 	logic [0: 6]	HEX2,
@@ -27,11 +27,20 @@ module blackjack_wrapper
 		output 	logic [0: 6]	HEX7
 	);
 
+	`hand	dealerHandSum;	//sum
+	`hand	playerHandSum; //sum
+	`gameState gameState, nextstate;
+	`turn turnTracker;
+
 	//TODO: fill in interface
-	blackjackGame theGame(CLOCK_50, SW[17], KEY[0], KEY[1], LEDR[17: 0], 
-						  LEDG[7: 0], HEX0[0: 6], HEX1[0: 6], HEX2[0: 6],
-						  HEX3[0: 6], HEX4[0: 6], HEX5[0: 6], HEX6[0: 6],
-						  HEX7[0: 6]);
+	blackjackGame theGame(CLOCK_50, SW[17], {KEY[2], KEY[1], KEY[0]}, 
+						  playerHandSum, dealerHandSum, gameState, turnTracker); 
+	
+	
+
+
+	outputController output(dealerHandSum, playerHandSum, gamestate, HEX7[0: 6], HEX6[0: 6], HEX5[0: 6], HEX4[0: 6], 
+							HEX3[0: 6], HEX2[0: 6], HEX1[0: 6], HEX0[0: 6]);
 
 	// Justin: I don't think switch 17 will be used for gameplay at all so
 	//		   it would make good for a reset switch
