8d8d0b07718f8206a293323c38ac78901232b870
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-11 23:34:21 -0700

    Update outputController.sv
    removed `;` from macros (the `;` gets included in the replacement)
    fixed `temp` latch
    Fixed getting 2A instead of 30

--- BlackJack project/outputController.sv
+++ BlackJack project/outputController.sv
@@ -10,43 +10,43 @@
 `include "gameState.svh"
 
 // make life visually easier
-`define SEGMENT_0 6'h0;
-`define SEGMENT_1 6'h1;
-`define SEGMENT_2 6'h2;
-`define SEGMENT_3 6'h3;
-`define SEGMENT_4 6'h4;
-`define SEGMENT_5 6'h5;
-`define SEGMENT_6 6'h6;
-`define SEGMENT_7 6'h7;
-`define SEGMENT_8 6'h8;
-`define SEGMENT_9 6'h9;
-`define SEGMENT_A 6'hA;
-`define SEGMENT_B 6'hB;
-`define SEGMENT_C 6'hC;
-`define SEGMENT_D 6'hD;
-`define SEGMENT_E 6'hE;
-`define SEGMENT_F 6'hF;
-`define SEGMENT_G 6'h10;
-`define SEGMENT_H 6'h11;
-`define SEGMENT_I 6'h12;
-`define SEGMENT_J 6'h13;
-`define SEGMENT_K 6'h14;
-`define SEGMENT_L 6'h15;
-`define SEGMENT_M 6'h16;
-`define SEGMENT_N 6'h17;
-`define SEGMENT_O 6'h18;
-`define SEGMENT_P 6'h19;
-`define SEGMENT_Q 6'h1A;
-`define SEGMENT_R 6'h1B;
-`define SEGMENT_S 6'h1C;
-`define SEGMENT_T 6'h1D;
-`define SEGMENT_U 6'h1E;
-`define SEGMENT_V 6'h1F;
-`define SEGMENT_W 6'h20;
-`define SEGMENT_X 6'h21;
-`define SEGMENT_Y 6'h22;
-`define SEGMENT_Z 6'h23;
-`define SEGMENT_OFF 6'h7F;
+`define SEGMENT_0 6'h0
+`define SEGMENT_1 6'h1
+`define SEGMENT_2 6'h2
+`define SEGMENT_3 6'h3
+`define SEGMENT_4 6'h4
+`define SEGMENT_5 6'h5
+`define SEGMENT_6 6'h6
+`define SEGMENT_7 6'h7
+`define SEGMENT_8 6'h8
+`define SEGMENT_9 6'h9
+`define SEGMENT_A 6'hA
+`define SEGMENT_B 6'hB
+`define SEGMENT_C 6'hC
+`define SEGMENT_D 6'hD
+`define SEGMENT_E 6'hE
+`define SEGMENT_F 6'hF
+`define SEGMENT_G 6'h10
+`define SEGMENT_H 6'h11
+`define SEGMENT_I 6'h12
+`define SEGMENT_J 6'h13
+`define SEGMENT_K 6'h14
+`define SEGMENT_L 6'h15
+`define SEGMENT_M 6'h16
+`define SEGMENT_N 6'h17
+`define SEGMENT_O 6'h18
+`define SEGMENT_P 6'h19
+`define SEGMENT_Q 6'h1A
+`define SEGMENT_R 6'h1B
+`define SEGMENT_S 6'h1C
+`define SEGMENT_T 6'h1D
+`define SEGMENT_U 6'h1E
+`define SEGMENT_V 6'h1F
+`define SEGMENT_W 6'h20
+`define SEGMENT_X 6'h21
+`define SEGMENT_Y 6'h22
+`define SEGMENT_Z 6'h23
+`define SEGMENT_OFF 6'h7F
 
 //`define FLASH_GREEN 8'hFF;
 //`define FLASH_RED  18'h3FFFF;
@@ -83,8 +83,6 @@ module outputController
 	logic [5:0] segmLetter1;
 	logic [5:0] segmLetter0;
 
-	logic [5:0] temp;
-
 	logic [6:0] playerValue7;
 	logic [6:0] playerValue6;
 
@@ -116,25 +114,22 @@ module outputController
 					playerHand7 = 5'd0;
 					playerHand6 = {1'b0, playerHand};
 				end
-			else if(playerHand <= 5'd20)
+			else if(playerHand <= 5'd19)
 				begin
 					//make left 7-seg 1 and right 7-seg playerHand - 10
 					playerHand7 = 5'd1;
-					temp = {1'b0, playerHand} - 5'd10;
-					playerHand6 = temp;
+					playerHand6 = {1'b0, playerHand} - 5'd10;
 				end
-			else if(playerHand <= 5'd30)
+			else if(playerHand <= 5'd29)
 				begin
 					//make left 7-seg 2 and right 7-seg playerHand - 20
 					playerHand7 = 5'd2;
-					temp = {1'b0, playerHand} - 5'd20;
-					playerHand6 = temp;
+					playerHand6 = {1'b0, playerHand} - 5'd20;
 				end		
 			else
 				begin //for the 1/100000 chance that the player hits at 20 and gets a 10
 					playerHand7 = 5'd3;
-					temp = {1'b0, playerHand} - 5'd30;
-					playerHand6 = temp;
+					playerHand6 = {1'b0, playerHand} - 5'd30;
 				end
 		end
 	
